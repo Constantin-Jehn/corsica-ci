80060fb9c2e850a7baa11dd16a421b69222ada19
LeonieRichterFAU <leonie.f.richter@fau.de>
2026-05-13 17:06:51 +0200

    plot works

--- src/CORSICA/artifact_reduction.py
+++ src/CORSICA/artifact_reduction.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import warnings
 from pathlib import Path
@@ -14,29 +16,36 @@ from mne.preprocessing import ICA
 from datetime import datetime
 
 
-def ci_artifact_reduction(raw, snr_threshold, fs_eeg, attended_audio, distraction_audio=None, peak_win_negative = 0.005, peak_win_pos = 0.012, output_dir= "output", plot=False, metadata=False):
+def ci_artifact_reduction(raw, subject_id, trial_id, output_dir, snr_threshold, fs_eeg, attended_audio, distraction_audio=None, peak_win_negative = 0.005, peak_win_pos = 0.012, plot=False, metadata=False):
     """
         Reduce CI Artifacts from EEG data
 
         Input:
         - raw: mne.io.Raw
 #         EEG data loaded from an EEGLAB .set file
+        - subject_id: str or int #TODO welches Format muss das haben?
+            Identifier for the subject, used for naming output files and saving metadata
+        - trial_id: str or int #TODO welches Format muss das haben?
+            Identifier for the trial, used for naming output files and saving metadata
+        - output_dir : str
+            Path where output files will be saved
         - snr_threshold: float 
             Maximum SNR value required for an independent component to not be extracted from the dataset
         - fs_eeg : int
             Sampling frequency of the EEG recording in Hz
         - attended_audio : np.ndarray
             1D NumPy array containing the signal of the attended audio stream
+
         - distraction_audio : np.ndarray
-            1D NumPy array containing the signal of the distracting (unattended) audio stream if available otherwise NaN
+            1D NumPy array containing the signal of the distracting (unattended) audio stream if available, default is None (for single speaker scenarios)
         - peak_win_negative : float
             Negative time lag window (in seconds), default 0.005s
         - peak_win_positive : float
             Positive time lag window (in seconds), default 0.012s
-        - output_dir : str
-            Directory where output files will be saved
         - plot : bool
-            If True, enables visualization of results, default is False
+            If True, enables saving of plots, default is False
+        - metadata : bool
+            If True, enables saving of metadata, default is False
 
         Returns:
         - eeg_cleaned: EEG data without CI Artifacts  
@@ -46,7 +55,14 @@ def ci_artifact_reduction(raw, snr_threshold, fs_eeg, attended_audio, distractio
         # DONE Check ob EEG und Audio attended/ distracted selbe länge haben
         # DONE Check obs ein single speaker oder conmpeting speaker ist, wenn competing ist audio attended + competing 
         # DONE Kommentare unterdrücken?
-        # - plot 
+        # DONE plot 
+        # - subject_id und trial_id einflechten und für richtige Speicherung nutzen
+        # - Kommentare schön machen
+
+    #Error if no output path
+    if output_dir is None:
+        raise ValueError("Output path must be specified.")
+    
     #prepare output directory
     if plot == True or metadata == True:
         output_dir = Path(output_dir) / "output_corsica"
@@ -54,15 +70,12 @@ def ci_artifact_reduction(raw, snr_threshold, fs_eeg, attended_audio, distractio
 
     #prepare audio_sum
     #set competing to true if necessary and adjust audio_sum    
-    competing = False #brauche ich diese Variable überhaupt wenn ich audio_sum sowieso direkt berechne? 
     audio_sum = attended_audio
     if isinstance(distraction_audio, np.ndarray):
-            competing = True
             audio_sum = attended_audio + distraction_audio
 
     #prepare eeg
     # load eeg with all components
-     #übergeben
     # load ica
     rank = np.linalg.matrix_rank(raw.get_data())
     print("rank:", rank)
@@ -83,31 +96,36 @@ def ci_artifact_reduction(raw, snr_threshold, fs_eeg, attended_audio, distractio
     if fs_eeg < 500:
         warnings.warn(
             f"Sampling frequency is very low (fs = {fs_eeg} Hz). "
-            "Results may be unreliable due to ...?",
+            "Only tested with frequencies >= 500 Hz. Results may be unreliable.",
             UserWarning
     )
 
     #iterate over all ic
     exclude = []
-    snrs= []
+    snr_s= []
+    lags_s = []
+    corr_s = []
     peak_in_seconds_after_stimulus_s = []
     for ic in range(ics.n_components_):
         corr = signal.correlate(ica_data[ic, :], audio_sum)
+        corr_s.append(corr)
         corr /= np.max(corr)
         lags = signal.correlation_lags(len(ica_data[ic, :]), len(audio_sum))
+        lags_s.append(lags)
         snr, central_lags, peak_value_idx, peak_in_seconds_after_stimulus = peak_snr(corr, fs_eeg, peak_win_negative, peak_win_pos)
-        snrs.append(snr)
+        snr_s.append(snr)
         peak_in_seconds_after_stimulus_s.append(peak_in_seconds_after_stimulus)
 
         # reject components based on snr value
-        if snr > snr_threshold: #wichtig
+        if snr > snr_threshold: 
             exclude.append(ic)
 
-        #if plot is true save plot 
-        if plot == True:
-            plotting(lags,corr,fs_eeg, snr, output_dir)
+        
     
-    
+    #if plot is true save plot 
+    if plot == True:
+        plotting(lags_s,corr_s,fs_eeg, snr_s, output_dir, subject_id, trial_id)
+
     # reconstruct EEG based on remaining ICs
     ics.exclude = exclude
     raw_cleaned = raw.copy()
@@ -118,7 +136,7 @@ def ci_artifact_reduction(raw, snr_threshold, fs_eeg, attended_audio, distractio
     
     
     if metadata == True: 
-        calculate_metadata(ics, exclude, snrs, peak_in_seconds_after_stimulus_s, output_dir)
+        calculate_metadata(ics, exclude, snr_s, peak_in_seconds_after_stimulus_s, output_dir)
 
     print ('ganze Methode durchgelaufen')
 
@@ -170,69 +188,92 @@ def check_dimensions(audio, eeg_data):
     else:
         print(f"Check successful: Arrays have the same length. Audio: {len(audio)}, EEG: {eeg_data.shape[1]}")
 
-def plotting(lags,corr,fs_eeg, snr, output_dir):
+def plotting(lags_s,corr_s,fs_eeg, snr_s, output_dir, subject_id, trial_id):
+    n = len(lags_s) 
+
+    n_cols = 4
+    n_rows = math.ceil(n / n_cols)
+    fig, axes = plt.subplots (n_rows, n_cols, figsize=(4* n_cols, 3*n_rows))
+    axes = axes.flatten()
     
-    #create plot
-    fig, ax = plt.subplots(1, 1, figsize=(6, 4.5))
+    for i in range (n):
+        ax = axes[i]
+        lags = lags_s[i]
+        corr = corr_s[i]
+        snr = snr_s[i]
 
-    # 1. Convert lags to ms (Assuming 'lags' is in seconds, * 1000)
-    lags_ms = (lags / 1000) * 1000 
+        # 1. Convert lags to ms (Assuming 'lags' is in seconds, * 1000)
+        lags_ms = (lags / 1000) * 1000 #NOTE wieso macht man hier geteilt durch und mal 1000
 
-    # 2. Main cross-correlation plot
-    ax.plot(lags_ms, corr, color='dimgrey', linewidth=1.5)
+        # 2. Main cross-correlation plot
+        ax.plot(lags_ms, corr, color='dimgrey', linewidth=1.5)
 
-    # 3. Mark the search window (-5ms to 15ms) in grey
-    ax.axvspan(-5, 15, color='grey', alpha=0.3, label='Search Window')
+        # 3. Mark the search window (-5ms to 15ms) in grey
+        ax.axvspan(-5, 15, color='grey', alpha=0.3, label='Search Window')
 
-    # 4. Highlight Peak within the window
-    # Ensure fs_eeg and corr are available in your local scope
-    n_samples = corr.shape[0]
-    start_idx = n_samples // 2 - int(0.005 * fs_eeg)
-    stop_idx = n_samples // 2 + int(0.015 * fs_eeg)
-    central_indices = np.arange(start_idx, stop_idx)
+        
+        # 4. Highlight Peak within the window
+        # Ensure fs_eeg and corr are available in your local scope
+        n_samples = corr.shape[0]
+        start_idx = n_samples // 2 - int(0.005 * fs_eeg)
+        stop_idx = n_samples // 2 + int(0.015 * fs_eeg)
+        central_indices = np.arange(start_idx, stop_idx)
 
-    peak_val = np.max(corr[central_indices])
-    peak_idx = np.argmax(corr[central_indices]) + start_idx
+        peak_val = np.max(corr[central_indices])
+        peak_idx = np.argmax(corr[central_indices]) + start_idx
 
-    ax.plot(lags_ms[peak_idx], peak_val, marker='x', color='black', markersize=8, mew=2)
+        ax.plot(lags_ms[peak_idx], peak_val, marker='x', color='black', markersize=8, mew=2)
 
-    # 5. Aesthetics: Limits, Labels, and Spines
-    ax.set_xlim([-300, 300])
-    ax.set_xlabel('Delay (ms)', fontsize=18)
-    ax.set_ylabel('Cross-correlation', fontsize=18)
-    ax.set_title("Correlation-based artifact rejection", fontsize=16, pad=15, weight='bold')
+        # 5. Aesthetics: Limits, Labels, and Spines
+        ax.set_xlim([-300, 300])
+        #ax.set_ylim([-1.1, 1.1])
+        ax.set_title(f"IC {i+1}", fontsize=12, fontweight='bold', pad=10)
+        ax.set_xlabel('Delay (ms)', fontsize=10)
+        ax.set_ylabel('Cross-correlation', fontsize=10)
+        #ax.set_title("Correlation-based artifact rejection", fontsize=16, pad=15, weight='bold')
 
-    ax.text(-200, 0.5, 'Search\nwindow', fontsize = 18)
-    ax.text(100, 0.5, f'Peak\nSNR={snr:.1f}dB', fontsize=18)
+        ax.text(-150, 0.6, 'Search\nwindow', fontsize = 12)
+        ax.text(100, 1.0, f'Peak\nSNR={snr:.1f}dB', fontsize=12)
 
-    # Remove upper and right spines
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
+        # Remove upper and right spines
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
 
-    ax.tick_params(axis='both', which='major', labelsize=14)
-
-    plt.tight_layout()
+        ax.tick_params(axis='both', which='major', labelsize=14)
     
-    plt.savefig(output_dir / f"test_plotting.svg")
-    #fig.savefig(os.path.join(output_path, 'TRF_corr_approach_search', f'method_1_calculation_{str(sub_id)}_{str(wav_name) + str(ic)}_{str(snr)}.svg')) #TODO
-    plt.show()
+    
+    # remove empty subplots
+    for j in range(n, len(axes)):
+        fig.delaxes(axes[j])
+
+    fig.suptitle("Correlation-based CI artifact reduction – cors-ica", fontsize=18, y=0.98)
+    fig.suptitle(f"Cross-Correlation plot of all Independent Components\n"
+             f"Subject: {subject_id} | Trial: {trial_id}", 
+             fontsize=16, fontweight='bold', y=0.98)
+
+    plt.tight_layout(rect=[0, 0, 1, 0.96])
+
+    #one pdf with everything
+    plt.savefig(output_dir / f"corsica_correlation_ic_subject_{subject_id}_trial_{trial_id}.pdf")
+
+    plt.close(fig)
 
     return 
 
-def calculate_metadata(ics, exclude, snrs, peak_in_seconds_after_stimulus_s, output_dir):
+def calculate_metadata(ics, exclude, snr_s, peak_in_seconds_after_stimulus_s, output_dir):
     #values to save
     number_of_ics= ics.n_components_
     number_excluded_ics = len(exclude) 
     percentage_remaining_ics = (number_of_ics - number_excluded_ics) / number_of_ics * 100
-    excluded_snr_values = [round(float(snrs[ic]), 3) for ic in exclude]
+    excluded_snr_values = [round(float(snr_s[ic]), 3) for ic in exclude]
     number_used_ics = number_of_ics - number_excluded_ics
     used_snr_indizes = [i for i in range(number_of_ics) if i not in exclude]
-    used_snr_values = [round(float(snrs[i]),3) for i in range(number_of_ics) if i not in exclude]
-    max_snr= max(snrs)
+    used_snr_values = [round(float(snr_s[i]),3) for i in range(number_of_ics) if i not in exclude]
+    max_snr= max(snr_s)
     excluded_peak_times_in_seconds_after_stimulus = [peak_in_seconds_after_stimulus_s[i] for i in exclude]
     mean_peak_in_seconds_after_stimulus_s = np.mean(excluded_peak_times_in_seconds_after_stimulus)
-    mean_snr = np.mean(snrs)
-    mean_snr_cleaned = np.mean([snrs[i] for i in range(number_of_ics) if i not in exclude])
+    mean_snr = np.mean(snr_s)
+    mean_snr_cleaned = np.mean([snr_s[i] for i in range(number_of_ics) if i not in exclude])
     mean_snr_excluded = np.mean(excluded_snr_values) if excluded_snr_values else float('nan')
 
     data = {
